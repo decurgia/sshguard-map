1115718641e7daf46f1cf666accb7f9b7752ff00
Gian Luca Decurtins <gldecurtins@icloud.com>
2022-06-27 14:46:02 +0200

    iso code

--- blacklistdump.py
+++ blacklistdump.py
@@ -3,21 +3,26 @@ import sqlite3
 import json
 import datetime
 
-parser = argparse.ArgumentParser(description='Dump coordinates into file.')
-parser.add_argument('-o', '--output',
-                    default='./markers.js',
-                    type=str,
-                    help='Path to markers.js file (default: ./markers.js)')
+parser = argparse.ArgumentParser(description="Dump coordinates into file.")
+parser.add_argument(
+    "-o",
+    "--output",
+    default="./markers.js",
+    type=str,
+    help="Path to markers.js file (default: ./markers.js)",
+)
 args = parser.parse_args()
 
-blacklist_db = sqlite3.connect('blacklist.sqlite')
+blacklist_db = sqlite3.connect("blacklist.sqlite")
 blacklist_cursor = blacklist_db.cursor()
-blacklist_cursor.execute('''SELECT latitude, longitude, organization, count() AS count
+blacklist_cursor.execute(
+    """SELECT latitude, longitude, organization, count() AS count
                FROM Blacklist
                WHERE latitude IS NOT NULL
                AND longitude IS NOT NULL
                GROUP BY latitude || longitude || organization
-               ORDER BY longitude, count DESC;''')
+               ORDER BY longitude, count DESC;"""
+)
 
 
 markers_dict = {}
@@ -27,14 +32,9 @@ for row in blacklist_cursor:
     organization = str(row[2])
     count = int(row[3])
     markerid = str(latitude) + str(longitude)
-    curmarker = markers_dict.get(markerid, (0, 0, '', 0))
-    description = (curmarker[2]
-                   + '\n'
-                   + organization
-                   + ' ('
-                   + str(count)
-                   + ')').strip()
-    description = description.replace('\n', '<br />')
+    curmarker = markers_dict.get(markerid, (0, 0, "", 0))
+    description = (curmarker[2] + "\n" + organization + " (" + str(count) + ")").strip()
+    description = description.replace("\n", "<br />")
     total = curmarker[3] + count
     markers_dict[markerid] = (latitude, longitude, description, total)
 blacklist_cursor.close()
@@ -42,21 +42,21 @@ blacklist_cursor.close()
 markers_list = []
 for entry in markers_dict.values():
     marker = {}
-    marker['latitude'] = entry[0]
-    marker['longitude'] = entry[1]
-    marker['description'] = entry[2]
-    marker['total'] = entry[3]
+    marker["latitude"] = entry[0]
+    marker["longitude"] = entry[1]
+    marker["description"] = entry[2]
+    marker["total"] = entry[3]
     markers_list.append(marker)
 
 markers_meta = {}
 dump_created_on = datetime.datetime.now()
-markers_meta['created_on'] = dump_created_on.strftime('%Y-%m-%d')
-markers_meta['count'] = len(markers_list)
+markers_meta["created_on"] = dump_created_on.strftime("%Y-%m-%d")
+markers_meta["count"] = len(markers_list)
 
-markers_file = open(args.output, 'w')
-markers_file.write('var markers = ' + json.dumps(markers_list, indent=4) + '\n\n')
-markers_file.write('var markers_meta = ' + json.dumps(markers_meta, indent=4))
+markers_file = open(args.output, "w")
+markers_file.write("var markers = " + json.dumps(markers_list, indent=4) + "\n\n")
+markers_file.write("var markers_meta = " + json.dumps(markers_meta, indent=4))
 markers_file.close()
 
 
-print('Dumped', len(markers_list), 'markers')
+print("Dumped", len(markers_list), "markers")
--- blacklistlookup.py
+++ blacklistlookup.py
@@ -7,17 +7,23 @@ import base64
 import sqlite3
 import json
 
-parser = argparse.ArgumentParser(description='Lookup coordinates of IP addresses.')
-parser.add_argument('-u', '--username', 
-                    dest='username',
-                    required=True,
-                    type=str,
-                    help='Maxmind username')
-parser.add_argument('-p', '--password',
-                    dest='password',
-                    required=True,
-                    type=str,
-                    help='Maxmind password')
+parser = argparse.ArgumentParser(description="Lookup coordinates of IP addresses.")
+parser.add_argument(
+    "-u",
+    "--username",
+    dest="username",
+    required=True,
+    type=str,
+    help="Maxmind username",
+)
+parser.add_argument(
+    "-p",
+    "--password",
+    dest="password",
+    required=True,
+    type=str,
+    help="Maxmind password",
+)
 
 args = parser.parse_args()
 
@@ -28,13 +34,13 @@ ctx.verify_mode = ssl.CERT_NONE
 
 # Sign-up at https://www.maxmind.com/en/geolite2/signup
 # Then generate username & password at https://www.maxmind.com/en/accounts/current/license-key
-b64auth = base64.standard_b64encode((args.username
-                                     + ":"
-                                     + args.password).encode("utf-8")).decode()
+b64auth = base64.standard_b64encode(
+    (args.username + ":" + args.password).encode("utf-8")
+).decode()
 
 
-def retrieve_latlongorg(ip_address):
-    url = ("https://geolite.info/geoip/v2.1/city/" + ip_address)
+def retrieve_latlongorgiso(ip_address):
+    url = "https://geolite.info/geoip/v2.1/city/" + ip_address
     request = urllib.request.Request(url)
     request.add_header("Authorization", "Basic " + b64auth)
     geoip_url = urllib.request.urlopen(request, context=ctx)
@@ -43,71 +49,90 @@ def retrieve_latlongorg(ip_address):
     latitude = 0.0
     longitude = 0.0
     organization = ""
+    iso_code = ""
     try:
-        latitude = geoip_json['location']['latitude']
-        longitude = geoip_json['location']['longitude']
-        organization = geoip_json['traits']['autonomous_system_organization']
+        latitude = geoip_json["location"]["latitude"]
+        longitude = geoip_json["location"]["longitude"]
+        organization = geoip_json["traits"]["autonomous_system_organization"]
+        iso_code = geoip_json["country"]["iso_code"]
     except KeyError as err:
-        print('KeyError', err)
+        print("KeyError", err)
         print(ip_address, geoip_json)
-    return[latitude, longitude, organization]
+    return [latitude, longitude, organization, iso_code]
 
 
-blacklist_db = sqlite3.connect('blacklist.sqlite')
+blacklist_db = sqlite3.connect("blacklist.sqlite")
 cur_single = blacklist_db.cursor()
 
 # First try to lookup the IPs without latitude and longitude
 blacklist_incomplete_list = blacklist_db.cursor()
-blacklist_incomplete_list.execute('''SELECT ip_address
+blacklist_incomplete_list.execute(
+    """SELECT ip_address
                FROM Blacklist
                WHERE latitude is NULL
                OR longitude is NULL
-               OR organization is NULL''')
+               OR organization is NULL
+               OR iso_code is NULL"""
+)
 
 for row in blacklist_incomplete_list:
     ip_address = str(row[0].strip())
     try:
-        latlongorg = retrieve_latlongorg(ip_address)
+        latlongorgiso = retrieve_latlongorgiso(ip_address)
     except urllib.error.HTTPError as err:
-        print('Lookup incomplete', err)
+        print("Lookup incomplete", err)
         blacklist_db.commit()
         quit()
-    cur_single.execute('''UPDATE Blacklist
-                        SET latitude = ?,
-                            longitude = ?,
-                            organization = ?
-                        WHERE ip_address = ?''',
-                       (latlongorg[0],
-                        latlongorg[1],
-                        latlongorg[2],
-                        ip_address))
+    cur_single.execute(
+        """UPDATE Blacklist
+                SET latitude = ?,
+                    longitude = ?,
+                    organization = ?,
+                    iso_code = ?
+                WHERE ip_address = ?""",
+        (
+            latlongorgiso[0],
+            latlongorgiso[1],
+            latlongorgiso[2],
+            latlongorgiso[3],
+            ip_address,
+        ),
+    )
 blacklist_db.commit()
 blacklist_incomplete_list.close()
 
 # Then try to update the ones which were updated the last
 blacklist_last_updated_list = blacklist_db.cursor()
-blacklist_last_updated_list.execute('''SELECT ip_address
+blacklist_last_updated_list.execute(
+    """SELECT ip_address
                FROM Blacklist
                WHERE updated_on < DATETIME('now', '-6 month')
-               ORDER BY updated_on ASC''')
+               ORDER BY updated_on ASC"""
+)
 
 for row in blacklist_last_updated_list:
     ip_address = str(row[0].strip())
     try:
-        latlongorg = retrieve_latlongorg(ip_address)
+        latlongorgiso = retrieve_latlongorgiso(ip_address)
     except urllib.error.HTTPError as err:
-        print('Lookup latest', err)
+        print("Lookup latest", err)
         blacklist_db.commit()
         quit()
-    cur_single.execute('''UPDATE Blacklist
-                        SET latitude = ?,
-                            longitude = ?,
-                            organization = ?
-                        WHERE ip_address = ?''',
-                       (latlongorg[0],
-                        latlongorg[1],
-                        latlongorg[2],
-                        ip_address))
+    cur_single.execute(
+        """UPDATE Blacklist
+                SET latitude = ?,
+                    longitude = ?,
+                    organization = ?,
+                    iso_code = ?
+                WHERE ip_address = ?""",
+        (
+            latlongorgiso[0],
+            latlongorgiso[1],
+            latlongorgiso[2],
+            latlongorgiso[3],
+            ip_address,
+        ),
+    )
 blacklist_db.commit()
 blacklist_last_updated_list.close()
 
